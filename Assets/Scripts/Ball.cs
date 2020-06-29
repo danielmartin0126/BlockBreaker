da34010b54b8fb8ca8e611e48dfeb6eb4bf3281b
danielmartin0126 <danielmartin0126@gmail.com>
2020-06-29 12:05:07 -0400

    Ball bounce sound effects
    Randomized sound from array of clips

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,16 +8,21 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
 
 
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    // Cached Component References
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
         
     }
 
@@ -44,5 +49,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
