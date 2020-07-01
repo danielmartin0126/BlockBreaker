377d5992356174f96cb90ed23a168b4c87b96200
danielmartin0126 <danielmartin0126@gmail.com>
2020-07-01 11:52:53 -0400

    Advance level
    Advances level after all blocks are destroyed

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,9 +7,26 @@ public class Level : MonoBehaviour
     // Serialize for debugging purposes
     [SerializeField] int breakableBlocks;
 
+    // cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
     }
 
 }
